9e51d1f176247f8af41c01f88736f9f56f64a13b
gabs38670 <gabriel.frodrigues15@senacsp.edu.br>
2026-06-26 21:11:32 -0300

    corrigindo problema no script.js do array artistas

--- 17-06/ex.02/script.js
+++ 17-06/ex.02/script.js
@@ -1,8 +1,24 @@
-const artista = ["Inês Brasil", "Grechen", "Andressa Urach",
-    "Valesca Popuzuda", "MC Mirella", "Anitta", "Pablo Vittar",
-    "Ludmilla", "Lexa", "Jojo Todynho"];
+const artistas = ["Inês Brasil", "Grechen", "Andressa Urach",
+   "Valesca Popuzuda", "MC Mirella", "Anitta", "Pablo Vittar",
+   "Ludmilla", "Lexa", "Jojo Todynho"];
 
-    // 1 - laço for
-    for (let i = 0; i < artista.length; i++){
-       document.getElementById("lista1").innerHTML += "<li>" + artista[i] + "</li>";
-    }
+// 1 - laço for
+for (let i = 0; i < artistas.length; i++) {
+   document.getElementById("lista1").innerHTML += "<li>" + artistas[i] + "</li>";
+}
+
+let i = 0;
+while (i < artistas.length) {
+   document.getElementById("lista2").innerHTML += "<li>" + artistas[i] + "</li>";
+   i++;
+}
+
+let j = 0;
+do {
+    document.getElementById("lista3").innerHTML += "<li>" + artistas[j] + "</li>";
+    j++;
+} while (j < artistas.length);
+
+artistas.forEach(function (artista) {
+    document.getElementById("lista4").innerHTML += "<li>" + artista + "</li>";
+});
